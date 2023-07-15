289986b2515ce2830b4858b82e240261fd6964b2
servet-isikli <servetisikli@yahoo.com>
2023-07-15 14:59:48 +0200

    first commit

---
+++ src/data/infoData.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 1,
+    name: "-",
+    link: "",
+    text: " ",
+    img: "https:",
+  },
+  {
+    id: 2,
+    name: "-",
+    link: "",
+    text: " ",
+    img: "https:",
+  },
+  {
+    id: 3,
+    name: "-",
+    link: "",
+    text: " ",
+    img: "https:",
+  },
+];
+
+export default data;
