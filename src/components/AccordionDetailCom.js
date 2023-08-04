7c814e13024873ce33f05eedc3165abc64d7294d
servet-isikli <servetisikli@yahoo.com>
2023-08-04 17:44:51 +0200

    update

--- src/components/AccordionDetailCom.js
+++ src/components/AccordionDetailCom.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Accordion, Alert, ListGroup } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import DataComponent from "../utils/DataComponent";
 import allData from "../data/data";
 
 const AccordionDetailCom = () => {
@@ -14,18 +13,31 @@ const AccordionDetailCom = () => {
 
   return (
     <div className="mt-3">
+      <h1>{allData[dataCategory][0].header}</h1>
       <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>{allData[dataCategory][0].header}</Accordion.Header>
-          <Accordion.Body>
-            <ListGroup>
-              <Alert variant="success" className="text-left mt-2">
-                {/* <Alert.Heading></Alert.Heading> */}
-                <DataComponent dataCategory={dataCategory} />
-              </Alert>
-            </ListGroup>
-          </Accordion.Body>
-        </Accordion.Item>
+        {data.map((item) => (
+          <Accordion.Item key={item.id} eventKey={item.id}>
+            <Accordion.Header>{item.name}</Accordion.Header>
+            <Accordion.Body>
+              <ListGroup>
+                <Alert variant="success" className="text-left mt-2">
+                  {/* <Alert.Heading></Alert.Heading> */}
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                  {item.link && (
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Daha fazla bilgi için tıklayın
+                    </a>
+                  )}
+                </Alert>
+              </ListGroup>
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
       </Accordion>
     </div>
   );
